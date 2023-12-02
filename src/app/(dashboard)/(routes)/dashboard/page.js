638d688dda2327b757ce1b89f75e77c23c42833e
Md. Mobashirul Alam Seam <mobashirulalamseam@gmail.com>
2023-12-02 21:18:10 +0600

    clerk authentication implemented

---
+++ src/app/(dashboard)/(routes)/dashboard/page.js
@@ -0,0 +1,13 @@
+// third party packages
+import { UserButton } from "@clerk/nextjs";
+
+const DashboardPage = () => {
+    return (
+        <div>
+            <p>DashboardPage</p>
+            <UserButton afterSignOutUrl="/" />
+        </div>
+    );
+};
+
+export default DashboardPage;
