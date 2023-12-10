278a60996fb85178bcc9a4b6898ccbf54e7287d9
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2023-12-10 15:49:25 +0600

    build: landing navbar && landing hero

--- src/app/(landing)/page.js
+++ src/app/(landing)/page.js
@@ -1,16 +1,11 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import LandingHero from "./_components/landing-hero";
+import LandingNavbar from "./_components/landing-navbar";
 
 const LandingPage = () => {
     return (
-        <div>
-            <p>Welcome to DEV.AI</p>
-            <Link href={"/sign-in"}>
-                <Button>Login</Button>
-            </Link>
-            <Link href={"/sign-up"}>
-                <Button>Register</Button>
-            </Link>
+        <div className="h-full">
+            <LandingNavbar />
+            <LandingHero />
         </div>
     );
 };
