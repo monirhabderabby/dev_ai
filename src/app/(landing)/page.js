5c9b9f689d8adc04d66d3d946dc64845e2d8dba5
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2023-12-10 15:54:44 +0600

    build: navbar content

--- src/app/(landing)/page.js
+++ src/app/(landing)/page.js
@@ -1,11 +1,14 @@
-import LandingHero from "./_components/landing-hero";
-import LandingNavbar from "./_components/landing-navbar";
+import dynamic from "next/dynamic";
+const LandingContent = dynamic(() => import("./_components/landing-content"));
+const LandingHero = dynamic(() => import("./_components/landing-hero"));
+const LandingNavbar = dynamic(() => import("./_components/landing-navbar"));
 
 const LandingPage = () => {
     return (
         <div className="h-full">
             <LandingNavbar />
             <LandingHero />
+            <LandingContent />
         </div>
     );
 };
